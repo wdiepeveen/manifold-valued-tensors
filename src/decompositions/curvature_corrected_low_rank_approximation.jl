22fb1c9dbc75113642a831603752d461995b8dde
wdiepeveen <willem_diep@hotmail.com>
2023-02-03 10:37:44 -0800

    Update curvature_corrected_low_rank_approximation.jl

--- src/decompositions/curvature_corrected_low_rank_approximation.jl
+++ src/decompositions/curvature_corrected_low_rank_approximation.jl
@@ -18,9 +18,7 @@ function curvature_corrected_low_rank_approximation(M, q, X, rank; stepsize=1/10
     ΣVtop = get_coordinates.(Ref(M), Ref(q), R_q, Ref(DefaultOrthonormalBasis()))
     Vtop = ΣVtop ./ Σ
     V = reduce(hcat,Vtop)
-
-    # TODO check whether we can get R_q back from these
-
+    
     # retain only top r components
     Ur = U[:,end-r+1:end]
     Σr = Σ[end-r+1:end]
