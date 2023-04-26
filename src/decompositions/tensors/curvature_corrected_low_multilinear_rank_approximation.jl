d0e040d7acb2231a8fb918e0a7d290601d2c0530
wdiepeveen <willem_diep@hotmail.com>
2023-04-26 15:12:54 +0100

    exact gradient error fix

--- src/decompositions/tensors/curvature_corrected_low_multilinear_rank_approximation.jl
+++ src/decompositions/tensors/curvature_corrected_low_multilinear_rank_approximation.jl
@@ -48,29 +48,6 @@ function curvature_corrected_low_multilinear_rank_approximation(M, q, X, rank)
         # TODO: report some error
     end
 
-    # A = zeros(d, r..., d, r...)
-    # b = zeros(d, r...)
-    # for jᵢ in J
-    #     ONBⱼᵢ = get_basis(M, q, DiagonalizingOrthonormalBasis(log_q_X[jᵢ]))
-    #     Θⱼᵢ = ONBⱼᵢ.data.vectors
-    #     κⱼᵢ = ONBⱼᵢ.data.eigenvalues
-
-    #     if typeof(M) <: AbstractSphere # bug in Manifolds.jl
-    #         κⱼᵢ .*= distance(M, q, X[jᵢ])^2
-    #     end
-
-    #     A += [sum([β(κⱼᵢ[j])^2 * inner(M, q, get_vector(M, q, (prod([U[i][jᵢ[i],l₁[i]] for i=1:dims]) .* Matrix(I, d, d))[:,k₁], DefaultOrthonormalBasis()), Θⱼᵢ[j]) * inner(M, q, get_vector(M, q, (prod([U[i][jᵢ[i],l₂[i]] for i=1:dims]) .* Matrix(I, d, d))[:,k₂], DefaultOrthonormalBasis()), Θⱼᵢ[j])  for j=1:d]) for k₁=1:d, l₁ in L, k₂=1:d, l₂ in L]
-    #     b += [sum([β(κⱼᵢ[j])^2 * inner(M, q, get_vector(M, q, (prod([U[i][jᵢ[i],l₁[i]] for i=1:dims]) .* Matrix(I, d, d))[:,k₁], DefaultOrthonormalBasis()), Θⱼᵢ[j]) * inner(M, q, log_q_X[jᵢ], Θⱼᵢ[j])  for j=1:d]) for k₁=1:d, l₁ in L]
-    # end
-
-    # # reshape
-    # AA = reshape(A, (d * prod(r), d * prod(r)))
-    # bb = reshape(b, (d * prod(r)))
-
-    # # solve linear system
-    # VVₖₗ = AA\bb
-    # Vₖₗ = reshape(VVₖₗ, (d, r...))
-
     # get ccRr_q
     ccR_q = get_vector.(Ref(M), Ref(q),[tensorVₖₗ[l[1],l[2],:] for l in L], Ref(DefaultOrthonormalBasis()))
     return ccR_q, U
--- src/functions/gradients/gradient_exact_loss.jl
+++ src/functions/gradients/gradient_exact_loss.jl
@@ -15,7 +15,7 @@ function gradient_exact_loss(M::AbstractManifold, q, X, U, V)
     # compute Euclidean gradients
     Vgradient = zeros(size(V))
     for i in 1:n
-        ONBᵢ = get_basis(M, q, DiagonalizingOrthonormalBasis(log_q_X[i]))
+        ONBᵢ = get_basis(M, q, DiagonalizingOrthonormalBasis(Ξ[i]))
         Θᵢ = ONBᵢ.data.vectors
         κᵢ = ONBᵢ.data.eigenvalues
         
@@ -26,7 +26,7 @@ function gradient_exact_loss(M::AbstractManifold, q, X, U, V)
         # compute (exp_q (Ξᵢ))
         qᵢ = exp(M, q, Ξ[i])
         
-        Vgradient += - 2 .* [sum([β(κᵢ[j]) * inner(M, q, get_vector(M, q, (U[i,l] .* Matrix(I, d, d))[:,k], DefaultOrthonormalBasis()), Θᵢ[j]) * inner(M, qᵢ, log(M, qᵢ, X[i]), parallel_transport_to(M, qᵢ, Θᵢ[j], q)) for j=1:d]) for k=1:d, l=1:r]
+        Vgradient += - 2 .* [sum([β(κᵢ[j]) * inner(M, q, get_vector(M, q, (U[i,l] .* Matrix(I, d, d))[:,k], DefaultOrthonormalBasis()), Θᵢ[j]) * inner(M, qᵢ, log(M, qᵢ, X[i]), parallel_transport_to(M, q, Θᵢ[j], qᵢ)) for j=1:d]) for k=1:d, l=1:r]
     end
 
     return Vgradient
